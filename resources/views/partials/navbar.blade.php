052536d13d7dc4f77d7a8668e07e79898ddf716f
alexaaqil <alexaaqil.se@gmail.com>
2024-08-20 22:47:26 +0300

    navbar: updated the content and styles of the navbar.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -8,43 +8,44 @@
     <div class="nav_links">
         @php
             $nav_links = [
+                ['route' => 'home', 'text' => 'Home'],
                 ['route' => 'about', 'text' => 'About'],
                 ['route' => 'services', 'text' => 'Services'],
                 ['route' => 'contact', 'text' => 'Contact'],
-                ['route' => 'users.blogs', 'text' => 'Blogs'],
+                // ['route' => 'users.blogs', 'text' => 'Blogs'],
             ];
         @endphp
 
-        @if(Auth::user() && Auth::user()->user_level == 1)
+        @if (Auth::user() && Auth::user()->user_level == 1)
             <a href="{{ route('dashboard') }}">Dashboard</a>
         @elseif(Auth::user() && Auth::user()->user_level == 0)
             <a href="{{ route('admin.dashboard') }}">Dashboard</a>
         @endif
 
-        @foreach($nav_links as $nav_link)
+        @foreach ($nav_links as $nav_link)
             <a href="{{ $nav_link['route'] ? route($nav_link['route']) : '#' }}" class="nav_link {{ Route::currentRouteName() === $nav_link['route'] ? 'active' : '' }}">
                 {{ $nav_link['text'] }}
             </a>
         @endforeach
     </div>
 
-    <div class="nav_authentication">
-        @if(Auth::user())
+    {{-- <div class="nav_authentication">
+        @if (Auth::user())
             <div class="actions">
                 <a href="{{ route('profile.edit') }}" class="profile">
                     <i class="fa fa-user"></i>
                 </a>
-    
+
                 <form action="{{ route('logout') }}" method="post">
                     @csrf
-    
+
                     <button type="submit" class="logout_btn">Logout</button>
                 </form>
             </div>
         @else
             <a href="{{ route('login') }}" class="login_btn">Login</a>
         @endif
-    </div>
+    </div> --}}
 
     <div class="burger_menu">
         <div class="burger_icon" id="burgerIcon">

64fa87fd1d21d4006cfb1a15d995bb1c092850ef
alexaaqil <alexaaqil.se@gmail.com>
2024-08-20 10:09:08 +0530

    homepage: Started the layout for the homepage.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,32 @@
 <x-general-layout class="Home">
     <section class="Hero">
         <div class="container">
-            <h1>Hero Section</h1>
+            <h1>Bolomba Metals</h1>
+        </div>
+    </section>
+
+    <section class="About">
+        <div class="container">
+            <p>Bolomba Metals is a diversified base metals mining company primarily producing copper, zinc, gold and nickel.</p>
+        </div>
+    </section>
+
+    <section class="Services">
+        <div class="container">
+            <h2>Services</h2>
+
+            <div class="services_container">
+                <div class="service">
+                    <div class="image">
+                        <img src="" alt="Mining">
+                    </div>
+
+                    <div class="text">
+                        <h3>Mining</h3>
+                        <p>We offer mining of minerals such as gold, diamonds for a wide range of clients.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </x-general-layout>

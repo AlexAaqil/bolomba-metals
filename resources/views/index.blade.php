6691deb0168089dab286ad311dd594f209a5273b
alexaaqil <alexaaqil.se@gmail.com>
2024-08-20 23:16:17 +0300

    homepage: added more content and styled the homepage.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,16 @@
 <x-general-layout class="Home">
     <section class="Hero">
         <div class="container">
-            <h1>Bolomba Metals</h1>
+            <h1>Leading the Future of Metals and Minerals Mining</h1>
         </div>
     </section>
 
     <section class="About">
         <div class="container">
-            <p>Bolomba Metals is a diversified base metals mining company primarily producing copper, zinc, gold and nickel.</p>
+            <p>At Bolomba Metals, we are a diversified base metals mining company with a focus on extracting and processing essential base metals including copper, zinc, gold, and nickel.</p>
+            <div class="btn">
+                <a href="{{ route('about') }}">Read More</a>
+            </div>
         </div>
     </section>
 
@@ -22,8 +25,31 @@
                     </div>
 
                     <div class="text">
-                        <h3>Mining</h3>
-                        <p>We offer mining of minerals such as gold, diamonds for a wide range of clients.</p>
+                        <h3>Mining Solutions</h3>
+                        <p>We specialize in the extraction of high-value minerals such as gold and diamonds, catering to a diverse range of global clients.</p>
+                    </div>
+                </div>
+
+                <div class="service">
+                    <div class="image">
+                        <img src="" alt="Mining">
+                    </div>
+
+                    <div class="text">
+                        <h3>Mineral Separation</h3>
+                        <p>Our advanced mineral separation and recovery services ensure maximum transparency and accuracy, delivering unparalleled results.</p>
+
+                    </div>
+                </div>
+
+                <div class="service">
+                    <div class="image">
+                        <img src="" alt="Mining">
+                    </div>
+
+                    <div class="text">
+                        <h3>Rare Mineral Sales</h3>
+                        <p>We offer carefully inspected, validated, and refined rare minerals to meet the exacting standards of our extensive client base.</p>
                     </div>
                 </div>
             </div>

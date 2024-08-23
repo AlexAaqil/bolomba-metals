83eb6bd7ade53c73e6229d347e5cda00f67e0559
alexaaqil <alexaaqil.se@gmail.com>
2024-08-23 23:17:21 +0300

    images and home page: added images to be used in the illustrations for services on the home page.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,8 @@
 
     <section class="About">
         <div class="container">
-            <p>At Bolomba Metals, we are a diversified base metals mining company with a focus on extracting and processing essential base metals including copper, zinc, gold, and nickel.</p>
+            <p>At Bolomba Metals, we are a diversified base metals mining company with a focus on extracting and
+                processing essential base metals including copper, zinc, gold, and nickel.</p>
             <div class="btn">
                 <a href="{{ route('about') }}">Read More</a>
             </div>
@@ -24,38 +25,41 @@
         <div class="container">
             <h2>Services</h2>
 
-            <div class="services_container">
+            <div class="services_wrapper">
                 <div class="service">
                     <div class="image">
-                        <img src="" alt="Mining">
+                        <img src="{{ asset('assets/images/mining.png') }}" alt="Mining">
                     </div>
 
                     <div class="text">
                         <h3>Mining Solutions</h3>
-                        <p>We specialize in the extraction of high-value minerals such as gold and diamonds, catering to a diverse range of global clients.</p>
+                        <p>We specialize in the extraction of high-value minerals such as gold and diamonds, catering to
+                            a diverse range of global clients.</p>
                     </div>
                 </div>
 
                 <div class="service">
                     <div class="image">
-                        <img src="" alt="Mining">
+                        <img src="{{ asset('assets/images/mineral-separation.png') }}" alt="Mineral Separation">
                     </div>
 
                     <div class="text">
                         <h3>Mineral Separation</h3>
-                        <p>Our advanced mineral separation and recovery services ensure maximum transparency and accuracy, delivering unparalleled results.</p>
+                        <p>Our advanced mineral separation and recovery services ensure maximum transparency and
+                            accuracy, delivering unparalleled results.</p>
 
                     </div>
                 </div>
 
                 <div class="service">
                     <div class="image">
-                        <img src="" alt="Mining">
+                        <img src="{{ asset('assets/images/rare-mineral-sales.png') }}" alt="Rare Mineral Sales">
                     </div>
 
                     <div class="text">
                         <h3>Rare Mineral Sales</h3>
-                        <p>We offer carefully inspected, validated, and refined rare minerals to meet the exacting standards of our extensive client base.</p>
+                        <p>We offer carefully inspected, validated, and refined rare minerals to meet the exacting
+                            standards of our extensive client base.</p>
                     </div>
                 </div>
             </div>

1b1f6d001bfb73ba51fecb2fc060789ef8759d82
alexaaqil <alexaaqil.se@gmail.com>
2024-09-03 10:09:57 +0530

    about page: added content to the about page and the styling as well.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,92 @@
 <x-general-layout class="About">
+    <div class="header">
+        <h1>About Us</h1>
+    </div>
+
     <div class="container">
-        <h1>About Page</h1>
+        <div class="content">
+            <p>We are a leading metals and rare minerals solutions service provider in the global economy. We pride ourselves being an all rounded rare minerals solutions service provider connecting the east and central africa region to the world at large.</p>
+            <p>We have an extensive track record across a wide range of commodities. Our teams have deep expertise in the mining, minerals and metals processing industries, as well as related non-process infrastructure such as energy solutions.</p>
+            <p>We deliver comporehensive advisory, engineering and project delivery services throughout the capital project lifecycle, from concept through to operational readiness and commissioning as well as ongoing operations, maintenance and engineering services. We do this with a focus on sustainability and assisting clients to achieve their ESG goals.</p>
+        </div>
+
+        <div class="content values">
+            <h2>Reasons for choosing us</h2>
+            <div class="values_wrapper">
+                <div class="value">
+                    <div class="image">
+                        <img src="" alt="">
+                    </div>
+                    <p class="title">People</p>
+                    <p>We value community as it is the cornerstone of our business.</p>
+                </div>
+
+                <div class="value">
+                    <div class="image">
+                        <img src="" alt="">
+                    </div>
+                    <p class="title">Safety</p>
+                    <p>We strive to deliver all projects with "zero harm".</p>
+                </div>
+
+                <div class="value">
+                    <div class="image">
+                        <img src="" alt="">
+                    </div>
+                    <p class="title">Integrity</p>
+                    <p>We strive to do what's right while still impacting economies and contributing to sustainable beneficial development.</p>
+                </div>
+
+                <div class="value">
+                    <div class="image">
+                        <img src="" alt="">
+                    </div>
+                    <p class="title">Excellence</p>
+                    <p>We're relentless in pursuing excellence. Every project is a chance to push boundaries, innovate, and redefine what's possible.</p>
+                </div>
+
+                <div class="value">
+                    <div class="image">
+                        <img src="" alt="">
+                    </div>
+                    <p class="title">Innovation</p>
+                    <p>We're not afraid to break new ground. We champion change, leading the way towards a more sustainable future for the mining industry.</p>
+                </div>
+
+                <div class="value">
+                    <div class="image">
+                        <img src="" alt="">
+                    </div>
+                    <p class="title">Trust</p>
+                    <p>We don't just deliver on our promises, but deliver on time.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="mission_vission">
+            <div class="text">
+                <h2>What sets us apart?</h2>
+                <div class="statements">
+                    <div class="statement">
+                        <p class="title">Our Mission</p>
+                        <p>To empower economies and enrich lives as well unlock the potential of East and Central Africa's resources, driving sustainable development for a brighter tomorrow.</p>
+                    </div>
+
+                    <div class="statement">
+                        <p class="title">Our Vision</p>
+                        <p>To turn the future of mining into reality as the most sought-after company in our field.</p>
+                    </div>
+
+                    <div class="statement">
+                        <p class="title">Purpose Statement</p>
+                        <p>To be creators of real value by fulfilling the aspirations of our people, clients, shareholders and communities.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="image">
+                <img src="{{ asset('assets/images/bolomba-metals-about.jpg') }}" alt="Bolomba Metals Purpose Statement">
+            </div>
+        </div>
     </div>
 </x-general-layout>

18de0454ba15c18c3402e79a9562358bc57cc6df
alexaaqil <alexaaqil.se@gmail.com>
2024-09-05 10:02:58 +0530

    services page: modified the design of the services page.

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -2,61 +2,61 @@
     <div class="jumbotron">
         <h1>Our Services</h1>
     </div>
-    
+
     <section class="Content">
         <div class="container">
             <p>The esteemed professionals at Bolomba Metals are unwaveringly dedicated to achieving engineering excellence at every juncture of the project journey. From inception to completion, and through commissioning, operations, and rehabilitation, our team is fueled by a blend of creativity and mathematical precision. This driving force enables us to not only tackle the challenges of today but also to anticipate and meet the demands of tomorrow with unparalleled innovation and expertise.</p>
         </div>
     </section>
-    
+
     <section class="Services_List">
         <div class="container cards_wrapper">
             <div class="service card">
                 <div class="image">
-                    <img src="{{ asset('assets/images/rare-minerals.jpg') }}" alt="Rare Minerals">
+                    <img src="{{ asset('assets/images/bolomba-metals-rare-minerals.jpg') }}" alt="Rare Minerals">
                 </div>
                 <div class="text">
-                    <h1>Rare Mineral Sales</h1>
+                    <h2>Rare Mineral Sales</h2>
                     <p>Our sales of rare minerals has undergone thorough inspection, validation and refinery to our large clientele.</p>
                 </div>
             </div>
-    
+
             <div class="service card">
                 <div class="image">
-                    <img src="" alt="Mineral Separation Services">
+                    <img src="{{ asset('assets/images/bolomba-metals-mineral-separation.jpg') }}" alt="Mineral Separation Services">
                 </div>
                 <div class="text">
-                    <h1>Mineral Separation Services</h1>
+                    <h2>Mineral Separation Services</h2>
                     <p>We offer unparalleled rare minerals separation and recovery services with utmost transparency and precision.</p>
                 </div>
             </div>
-    
+
             <div class="service card">
                 <div class="image">
-                    <img src="" alt="Mining">
+                    <img src="{{ asset('assets/images/bolomba-metals-hero.jpg') }}" alt="Mining">
                 </div>
                 <div class="text">
-                    <h1>Mining</h1>
+                    <h2>Mining</h2>
                     <p>We offer mining of minerals such as gold, diamonds for a wide range of clients.</p>
                 </div>
             </div>
-    
+
             <div class="service card">
                 <div class="image">
-                    <img src="{{ asset('assets/images/consulting.jpg') }}" alt="Consulting">
+                    <img src="{{ asset('assets/images/bolomba-metals-consultation.jpg') }}" alt="Consulting">
                 </div>
                 <div class="text">
-                    <h1>Consulting</h1>
+                    <h2>Consulting</h2>
                     <p>Our consultancy consists of project assessment/evaluation, economic evaluation, design criteria development.</p>
                 </div>
             </div>
-    
+
             <div class="service card">
                 <div class="image">
-                    <img src="{{ asset('assets/images/dedicated-support.jpg') }}" alt="Dedicated Support">
+                    <img src="{{ asset('assets/images/bolomba-metals-dedicated-support.jpg') }}" alt="Dedicated Support">
                 </div>
                 <div class="text">
-                    <h1>Dedicated Support</h1>
+                    <h2>Dedicated Support</h2>
                     <p>Teams are kept small with the best skills for projects to provide clients with dedicated support from beginning to end.</p>
                 </div>
             </div>
